e51deb6434b039886602b5823e49ddbdb35203f8
Tony Wael <leaw@live.nl>
2014-02-12 11:50:09 +0100

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,8 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
+  config.include Capybara::DSL # listing 3.10 Adding the Capybara DSL to the RSpec helper file. 
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
