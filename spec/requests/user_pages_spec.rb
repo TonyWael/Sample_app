67a2f66173f9a9c4802df7b0a48575f19248817c
Tony Wael <leaw@live.nl>
2014-02-26 19:32:29 +0100

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,22 @@ describe "User pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
 
   describe "index" do
     let(:user) { FactoryGirl.create(:user) }
@@ -96,9 +112,6 @@ describe "User pages" do
       it { should have_content('error') }
     end
   end
-
-
-
 
   describe "signup page" do
     before { visit signup_path }
