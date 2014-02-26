60a4b49812b0947f5691640517003d2ef693e62c
Tony Wael <leaw@live.nl>
2014-02-26 14:22:24 +0100

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-      password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -13,11 +12,24 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+    it { should respond_to(:admin) }
 
   it { should be_valid }
+    it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+      before do
+        @user.save!
+        @user.toggle!(:admin)
+      end
+
+      it { should be_admin }
+    end
+
+
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -65,19 +77,6 @@ describe User do
       it { should_not be_valid }
     end
 
-    describe "when password is not present" do
-      before do
-        @user = User.new(name: "Example User", email: "user@example.com",
-          password: " ", password_confirmation: " ")
-      end
-      it { should_not be_valid }
-    end
-
-    describe "when password doesn't match confirmation" do
-      before { @user.password_confirmation = "mismatch" }
-      it { should_not be_valid }
-    end
-
     describe "with a password that's too short" do
       before { @user.password = @user.password_confirmation = "a" * 5 }
       it { should be_invalid }
@@ -100,7 +99,8 @@ describe User do
     end
 
     describe "remember token" do
-      before { @user.save }
-      its(:remember_token) { should_not be_blank }
-    end
+        before { @user.save }
+        its(:remember_token) { should_not be_blank }
+      end
+
   end
