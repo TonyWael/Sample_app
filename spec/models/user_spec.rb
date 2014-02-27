d37ad5e955711d7cf52905c99005f521ff6aa739
Tony Wael <leaw@live.nl>
2014-02-27 15:34:45 +0100

    Add user following fix but 8 unpassing test over

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,10 +66,21 @@ describe User do
         let(:unfollowed_post) do
           FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
         end
+        let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
         its(:feed) { should include(newer_micropost) }
         its(:feed) { should include(older_micropost) }
         its(:feed) { should_not include(unfollowed_post) }
+        its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
       end
   end
 
@@ -161,7 +172,7 @@ describe "following" do
       subject { other_user }
       its(:followers) { should include(@user) }
     end
-    
+
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
    
